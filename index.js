d3e7eda745cec05c18956c694e243f3e268f6d4d
SiriusLL <sirius.love.light@gmail.com>
2021-03-12 13:07:00 +0000

    save

--- index.js
+++ index.js
@@ -1,37 +1,39 @@
-const { fetchMyIP, fetchCoordsByIP, fetchISSFlyOverTimes } = require('./iss');
+//const { fetchMyIP, fetchCoordsByIP, fetchISSFlyOverTimes } = require('./iss');
 // const { fetchCoordsByIP } = require('./iss');
 // const { fetchISSFlyOverTimes } = require('./iss');
 
-fetchMyIP((error, ip) => {
-  if (error) {
-    console.log("It didn't work!" , error);
-    return;
-  }
+// fetchMyIP((error, ip) => {
+//   if (error) {
+//     console.log("It didn't work!" , error);
+//     return;
+//   }
 
-  console.log('It worked! Returned IP:' , ip);
+//   console.log('It worked! Returned IP:' , ip);
 
-  fetchCoordsByIP(ip, (error, coords) => {
-    if (error) {
-      console.log("It didnt'work!" , error);
-      return;
-    }
   
-    console.log("It worked!!")
-    console.log('the coords are', coords);
+// });
+
+
+// fetchCoordsByIP(ip, (error, coords) => {
+//   if (error) {
+//     console.log("It didnt'work!" , error);
+//     return;
+//   }
+
+//   console.log("It worked!!")
+//   console.log('the coords are', coords);
+
   
-    fetchISSFlyOverTimes(coords, (error, passTimes) => {
-      if (error) {
-        console.log("It didn't work!", error);
-        return;
-      }
-  
-      console.log("it worked!, returned flyover times:", passTimes);
-    });
-  });
-});
+// });
 
+// const exampleCoords = { latitude: '49.27670', longitude: '-123.13000' };
+// fetchISSFlyOverTimes(coords, (error, passTimes) => {
+//   if (error) {
+//     console.log("It didn't work!", error);
+//     return;
+//   }
 
-
-
+//   console.log("it worked!, returned flyover times:", passTimes);
+// });
 
 
